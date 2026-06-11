0d234119fbf4aff529b007c22fd1652a14546eeb
Francesco <francescoioli@cnr.it>
2026-06-11 12:26:11 +0200

    add Cache-Control header in config.php

--- app/camera-viewer/config.php
+++ app/camera-viewer/config.php
@@ -1,4 +1,5 @@
 <?php
 header('Content-Type: application/javascript');
+header('Cache-Control: no-store');
 $api_base = getenv('API_BASE') ?: 'http://localhost:8000';
 echo 'const API_BASE = ' . json_encode($api_base) . ';';
